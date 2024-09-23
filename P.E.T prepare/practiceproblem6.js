edb8614d0b1af7943ebf41962e35bf789c2ff730
Madasamy123 <madasamimadasami2002@gmail.com>
2024-09-23 23:55:17 +0530

    .

--- P.E.T prepare/practiceproblem6.js
+++ P.E.T prepare/practiceproblem6.js
@@ -14,3 +14,11 @@ for(let i=1;r>=i;r--){
 
 //12. Write a program where a robot must complete 10 laps around a circuit. Use a while loop to count the laps 
 //and display the lap number each time the robot completes one.
+
+let i=1;
+
+while(i<=10){
+  
+   console.log("robot completes"+"  "+i)
+   i++;
+}
