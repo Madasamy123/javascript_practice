66c77f64927f1bb8ebec7c1a20f52fb2afe35666
Madasamy123 <madasamimadasami2002@gmail.com>
2025-01-21 23:10:34 +0530

    practice call stack

---
+++ level6/practice.js
@@ -0,0 +1,19 @@
+function a(){
+    console.log("hello!")
+    b();
+    console.log("You like for one language?")
+}
+
+    function b(){
+        console.log("What is your name?");
+        c();
+        console.log("Think about learning for this one month?")
+
+    }
+
+        function c(){
+            console.log("Where are you from?")
+        }
+    
+a();
+
